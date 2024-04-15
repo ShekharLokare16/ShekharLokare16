7181f1c1306e69691addb9e5b9b60933ef55d289
ShekharLokare16 <167084665+ShekharLokare16@users.noreply.github.com>
2024-04-15 17:51:15 +0530

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+#This script will return the users which have read acces in your repo
 
 # GitHub API URL
 API_URL="https://api.github.com"
 
-# GitHub username and personal access token
-USERNAME=$username
-TOKEN=$token
+# GitHub username and personal access token for authrntication
+USERNAME=$username # your Git Username
+TOKEN=$token # your Git Token
 
 # User and Repository information
-REPO_OWNER=$1
-REPO_NAME=$2
+# while running the script run following command
+# ./list-users.sh ShekharLokare16 ShekharLokare16 (org_name repo_name)
+REPO_OWNER=$1 # org_name
+REPO_NAME=$2  # repo_name
 
 # Function to make a GET request to the GitHub API
 function github_api_get {
@@ -25,7 +28,7 @@ function list_users_with_read_access {
     local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
     # Fetch the list of collaborators on the repository
-    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')" # as script uses jq u have to install jq using  sudo apt install jq -y
 
     # Display the list of collaborators with read access
     if [[ -z "$collaborators" ]]; then
